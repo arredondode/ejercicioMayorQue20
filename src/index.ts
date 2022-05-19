6c9595eb99e7f481dd503208874cf80c13e8c1ae
arredondode <104038069+arredondode@users.noreply.github.com>
2022-05-19 12:17:28 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let numero: number = Number(prompt("Ingrese el numero"));
+
+if (numero > 20) {
+  console.log("El número " + numero + " es mayor que 20");
+} else {
+  console.log("El numero" + numero + " es menor o igual a 20");
+}
